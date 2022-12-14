8d513e398f95c9aa40d3cbe76cc9953d3bbdcb61
Qiping Pan <panqp@gl-login1.arc-ts.umich.edu>
2022-12-13 22:25:53 -0500

    second context run

--- start.sh
+++ start.sh
@@ -14,4 +14,4 @@ source env/bin/activate
 
 # run job
 pwd
-jupyter nbconvert --to notebook --execute Verifiable-Coherent-NLU.ipynb --output roberta-context
+jupyter nbconvert --to notebook --execute Verifiable-Coherent-NLU.ipynb --output roberta-context-2
